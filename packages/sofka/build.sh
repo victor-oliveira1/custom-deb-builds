371d573d38d68476d63ccdeaf96783a386f4148e
Victor Oliveira <victor.oliveira@semeartech.com.br>
2026-09-17 16:45:16 -0300

    Adding missing dependencies to sofka package

--- packages/sofka/build.sh
+++ packages/sofka/build.sh
@@ -5,6 +5,8 @@ PKG_NAME=$(awk -F'/' '{print $(NF-1)}' <<<"$0")
 VERSION="$1"
 OUTPUT_DIR="$2"
 
+apt update && apt install -y jq curl
+
 URL=$(curl -s "https://api.github.com/repos/nklmilojevic/sofka/releases/latest" | jq -r '.assets[] | select(.name | endswith(".deb") and contains("amd64")) | .browser_download_url')
 curl -LO "$URL"
 cp *.deb "$OUTPUT_DIR/"
