082d480b9578d324ee1223ae989b065e0f8157bd
Victor Oliveira <victor.oliveira@semeartech.com.br>
2026-09-17 09:20:51 -0300

    Adding pcloud package

--- packages/pcloudcc/build.sh
+++ packages/pcloudcc/build.sh
@@ -1,39 +1,47 @@
 #!/usr/bin/env bash
 set -e
 
+echo "0: $0"
+echo "SOURCE: ${BASH_SOURCE[0]}"
 VERSION="$1"
 OUTPUT_DIR="$2"
 
 # 1. Instala dependências específicas deste app
-apt-get update && apt-get install -y git sudo lsb-release equivs golang libpng-dev libx11-dev libxcb1-dev
+apt-get update && apt-get install -y git \
+                                     sudo \
+                                     equivs \
+                                     libfuse3-dev \
+                                     libreadline-dev \
+                                     libmbedtls-dev \
+                                     libsqlite3-dev \
+                                     libudev-dev
 
 # 2. Clona e compila
-git clone https://github.com/d99kris/nchat build_src
+git clone https://github.com/lneely/pcloudcc-lneely build_src
 cd build_src
 git checkout "$VERSION"
 
-./make.sh deps -y
-NCHAT_CMAKEARGS="-DCMAKE_INSTALL_PREFIX=/usr" ./make.sh build --no-telegram
-cd build
-make install DESTDIR=./prefix
+make -j$(nproc)
+mkdir -p prefix/usr/bin
+cp pcloudcc prefix/usr/bin/
 
 # 3. Gera o arquivo control do equivs
-cat <<EOF > nchat.control
+cat <<EOF > pcloudcc.control
 Section: net
 Priority: optional
 Standards-Version: 3.9.2
 
-Package: nchat
-Version: ${VERSION#v}
+Package: pcloudcc
+Version: 0.0~git$(date +%Y%m%d).${VERSION}-1
 Maintainer: Victor Oliveira <victor.oliveira@gmx.com>
 Architecture: amd64
-Depends: libreadline8, libssl3, libncursesw6, libsqlite3-0, libmagic1, libxcb1
-Files:$(while read L; do D=${L%/*}; echo " ./prefix$L $D/"; done < install_manifest.txt)
-Description: Terminal-based Telegram and WhatsApp client
- nchat is a terminal-based chat client with support for
- Telegram and WhatsApp. It features a modern ncurses UI.
+Depends: $(find prefix -type f -exec dpkg-shlibdeps -O {} + 2>/dev/null | sed -e 's/shlibs:Depends=//' -e 's/\n/,/g' -e 's/,,*/,/g; s/^,//; s/,$//')
+Files:$(while read FILE; do FILE_DIR="${FILE%/*}"; echo " ${FILE} ${FILE_DIR#*prefix}/"; done < <(find prefix/ -mindepth 2 -type f))
+Description: pcloudcc-lneely is an independent fork of the inactive pcloudcom/console-client
+ .
+ It's used to mount pCloud folder as a local directory.
 EOF
 
 # 4. Empacota e move para o diretório de saída global
-DEB_BUILD_OPTIONS="nostrip nodwz" equivs-build nchat.control
+DEB_BUILD_OPTIONS="nostrip nodwz" equivs-build pcloudcc.control
 cp *.deb "$OUTPUT_DIR/"
--- packages/pcloudcc/check.sh
+++ packages/pcloudcc/check.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -s https://github.com/d99kris/nchat/releases.atom | grep -oP '(?<=/tag/)[^"]+' | head -n 1
+curl -s https://github.com/lneely/pcloudcc-lneely/releases.atom | grep -oP '(?<=/tag/)[^"]+' | head -n 1
